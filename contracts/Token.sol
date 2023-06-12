c3919686fdde3b8c6aee55a622437e9d9da19248
Szymon Ligas <szli6792@colorado.edu>
2023-06-12 03:04:08 -0600

    7. Added ability to delegate token transfers for this ERC20 token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+// A SIMPLE ERC20 TOKEN:
+
 contract Token { // contains all smart contract code
     // name is invoked in hardhat using .name() function call after .getContractAt() function call
     // In ethereum, Token#name is a contract call
@@ -47,21 +49,32 @@ contract Token { // contains all smart contract code
             balanceOf[msg.sender] = totalSupply; // Take all the tokens and assign them to deployment address
         }
 
-    // Allow simple sending of tokens
+    // Internal transfer utility (private function)
+    function _transfer(
+        address _from,
+        address _to, 
+        uint256 _value
+    )
+    internal {
+        // Require that the receiver is not the 0 address
+        require(_to != address(0)); // (prevents burning via the transfer function)
+        // Deduct tokens from spender
+        balanceOf[_from] = balanceOf[_from] - _value; // read balance of sender and subtract the outgoing value
+        // Credit tokens to receiver
+        balanceOf[_to] = balanceOf[_to] + _value; // read balance of receiver and add the incoming value
+        // Emit event
+        emit Transfer(_from, _to, _value);
+    }
+
+    // Allow simple (direct) sending of tokens by any token holder
     function transfer(
         address _to, 
         uint256 _value) 
     public returns (bool success) {
         // Require that sender has enough tokens to spend
         require(balanceOf[msg.sender] >= _value);
-        // Require that the receiver is not the 0 address
-        require(_to != address(0)); // (prevents burning via the transfer function)
-        // Deduct tokens from spender
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value; // read balance of sender and subtract the outgoing value
-        // Credit tokens to receiver
-        balanceOf[_to] = balanceOf[_to] + _value; // read balance of receiver and add the incoming value
-        // Emit event
-        emit Transfer(msg.sender, _to, _value);
+        // Call internal funciton
+        _transfer(msg.sender,_to,_value);
 
         return true;
     }
@@ -75,6 +88,24 @@ contract Token { // contains all smart contract code
         allowance[msg.sender][_spender] = _value;
         // Emit event
         emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // Allow delegated sending of tokens (after approval) by any token
+    function transferFrom(
+        address _from,
+        address _to, 
+        uint256 _value) 
+    public returns (bool success) {
+        // Check approval
+        require(allowance[_from][msg.sender] >= _value);
+        // Require that sender has enough tokens to spend
+        require(balanceOf[_from] >= _value);
+        // Reset allowance
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value; // Takes away whatever part of allowance was spent
+        // Call internal funciton
+        _transfer(_from,_to,_value);
 
         return true;
     }
--- test/Token.js
+++ test/Token.js
@@ -34,54 +34,53 @@ describe('Token', () => {
         it('Has the correct name', async () => {
 
             // Check that name is correct
-            expect(await token.name()).to.equal(name)
+            expect(await token.name()).to.be.equal(name)
             console.log(`\t Token name: ${await token.name()}`)
         })
     
         it('Has the correct symbol', async () => {
     
             // Check that  symbol is correct
-            expect(await token.symbol()).to.equal(symbol)
+            expect(await token.symbol()).to.be.equal(symbol)
             console.log(`\t Token symbol: ${await token.symbol()}`)
         })
     
         it('Has the correct decimals', async () => {
     
             // Check that decimals is correct
-            expect(await token.decimals()).to.equal(decimals)
+            expect(await token.decimals()).to.be.equal(decimals)
             console.log(`\t Token decimals: ${await token.decimals()}`)
         })
     
         it('Has the correct total supply', async () => {
     
             // Check that supply is correct
-            expect(await token.totalSupply()).to.equal(totalSupply)
+            expect(await token.totalSupply()).to.be.equal(totalSupply)
             console.log(`\t Token supply: ${await token.totalSupply()}`)
         })
 
         it('Assigns token supply to deployer', async () => {
     
             // Check that supply is funded to deployer
-            expect(await token.balanceOf(deployer)).to.equal(totalSupply)
+            expect(await token.balanceOf(deployer)).to.be.equal(totalSupply)
             console.log(`\t Deployer balance: ${await token.balanceOf(deployer)}`)
         })
     })
 
-    describe('Token transfer', () => {
+    describe('Direct Token Transfer', () => {
         let amount, transaction, result, totalSupply
 
         describe('Test a valid transfer:', () => {
 
             beforeEach(async () => {
                 amount = tokens(100)
-
                 totalSupply = await token.totalSupply()
 
                 // Log balances before transfer
-                expect(await token.balanceOf(deployer)).to.equal(BigInt(totalSupply))
+                expect(await token.balanceOf(deployer)).to.be.equal(BigInt(totalSupply))
                 console.log(`\t   Deployer balance before: ${await token.balanceOf(deployer)}`)
 
-                expect(await token.balanceOf(receiver)).to.equal(0)
+                expect(await token.balanceOf(receiver)).to.be.equal(0)
                 console.log(`\t   Receiver balance before: ${await token.balanceOf(receiver)}`)
 
                 // Make a token transfer
@@ -92,25 +91,25 @@ describe('Token', () => {
 
             it('Transfers token balance', async () => {
                 // Log balances after transfer
-                expect(await token.balanceOf(deployer)).to.equal(BigInt(totalSupply) - amount)
+                expect(await token.balanceOf(deployer)).to.be.equal(BigInt(totalSupply) - amount)
                 console.log(`\t   Deployer balance after: ${await token.balanceOf(deployer)}`)
 
-                expect(await token.balanceOf(receiver)).to.equal(amount)
+                expect(await token.balanceOf(receiver)).to.be.equal(amount)
                 console.log(`\t   Receiver balance after: ${await token.balanceOf(receiver)}`)
             })
 
             it('Emits a transfer event', async () => {
 
                 // Check event name
-                expect(result.logs[0].fragment.name).to.equal('Transfer')
+                expect(result.logs[0].fragment.name).to.be.equal('Transfer')
                 console.log(`\t   Event Name: ${result.logs[0].fragment.name}`)
 
                 // Check event details
-                expect(result.logs[0].args.from).to.equal(await deployer.getAddress())
+                expect(result.logs[0].args.from).to.be.equal(await deployer.getAddress())
                 console.log(`\t   Sender Address: ${result.logs[0].args.from}`)
-                expect(result.logs[0].args.to).to.equal(await receiver.getAddress())
+                expect(result.logs[0].args.to).to.be.equal(await receiver.getAddress())
                 console.log(`\t   Receiver Address: ${result.logs[0].args.to}`)
-                expect(result.logs[0].args.value).to.equal(amount)
+                expect(result.logs[0].args.value).to.be.equal(amount)
                 console.log(`\t   Amount Sent: ${result.logs[0].args.value}`)
             })
         })
@@ -128,7 +127,7 @@ describe('Token', () => {
         })
     })
     describe('Token Approval', () => {
-        let amount, transaction, result, totalSupply
+        let amount, transaction, result
 
         beforeEach(async () => {
             amount = tokens(100)
@@ -141,21 +140,21 @@ describe('Token', () => {
         describe('Test a valid approval:', () => {
 
             it('Allocates an allowance for delegated token spending', async () => {
-                expect(await token.allowance(deployer, exchange)).to.equal(amount)
+                expect(await token.allowance(deployer, exchange)).to.be.equal(amount)
             })
 
             it('Emits an approval event', async () => {
 
                 // Check event name
-                expect(result.logs[0].fragment.name).to.equal('Approval')
+                expect(result.logs[0].fragment.name).to.be.equal('Approval')
                 console.log(`\t   Event Name: ${result.logs[0].fragment.name}`)
 
                 // Check event details
-                expect(result.logs[0].args.owner).to.equal(await deployer.getAddress())
+                expect(result.logs[0].args.owner).to.be.equal(await deployer.getAddress())
                 console.log(`\t   Owner Address: ${result.logs[0].args.owner}`)
-                expect(result.logs[0].args.spender).to.equal(await exchange.getAddress())
+                expect(result.logs[0].args.spender).to.be.equal(await exchange.getAddress())
                 console.log(`\t   Spender Address: ${result.logs[0].args.spender}`)
-                expect(result.logs[0].args.value).to.equal(amount)
+                expect(result.logs[0].args.value).to.be.equal(amount)
                 console.log(`\t   Amount Approved: ${result.logs[0].args.value}`)
             })
         })
@@ -163,6 +162,64 @@ describe('Token', () => {
         describe('Test an invalid approval:', () => {
             it('Rejects invalid spenders', async () => {
                 await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+            })
+        })
+    })
+    describe('Delegated Token Transfer', () => {
+        let amount, transaction, result, totalSupply
+
+        beforeEach(async () => {
+            amount = tokens(100)
+            totalSupply = await token.totalSupply()
+
+            // Make a token transfer
+            transaction = await token.connect(deployer).approve(exchange, amount) // connects deployer wallet to write to blockchain (pay the gas fee to sign which prevents network spam)
+            result = await transaction.wait() // wait for transaction receipt
+        })
+
+        describe('Test a valid transfer:', () => {
+
+            beforeEach(async () => {
+
+                // Exchange facilitates one part of a swap
+                transaction = await token.connect(exchange).transferFrom(deployer, receiver, amount)
+                result = await transaction.wait()
+            })
+
+            it('Transfers token balance', async () => {
+
+                // Log balances after transfer
+                expect(await token.balanceOf(deployer)).to.be.equal(BigInt(totalSupply) - amount)
+                console.log(`\t   Deployer balance after: ${await token.balanceOf(deployer)}`)
+
+                expect(await token.balanceOf(receiver)).to.be.equal(amount)
+                console.log(`\t   Receiver balance after: ${await token.balanceOf(receiver)}`)
+
+            })
+
+            it('Resets the allowance', async () => {
+                expect(await token.allowance(deployer, exchange)).to.be.equal(0)
+            })
+
+            it('Emits a transfer event', async () => {
+
+                // Check event name
+                expect(result.logs[0].fragment.name).to.be.equal('Transfer')
+                console.log(`\t   Event Name: ${result.logs[0].fragment.name}`)
+
+                // Check event details
+                expect(result.logs[0].args.from).to.be.equal(await deployer.getAddress())
+                console.log(`\t   Sender Address: ${result.logs[0].args.from}`)
+                expect(result.logs[0].args.to).to.be.equal(await receiver.getAddress())
+                console.log(`\t   Receiver Address: ${result.logs[0].args.to}`)
+                expect(result.logs[0].args.value).to.be.equal(amount)
+                console.log(`\t   Amount Sent: ${result.logs[0].args.value}`)
+            })
+        })
+        describe('Test an invalid transfer:', () => {
+            it('Reject unapproved transfer', async () => {
+                // Attempt to transfer too many tokens
+                await expect(token.connect(exchange).transferFrom(deployer, receiver, tokens(10000000000))).to.be.reverted
             })
         })
     })

e17a72edfb75377b88405b4f19ec5fc67d9acf6c
Szymon Ligas <szli6792@colorado.edu>
2023-06-12 03:31:24 -0600

    7.5 Updated ERC20 Token Contract Comments

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,29 +3,32 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-// A SIMPLE ERC20 TOKEN:
+// A SIMPLE ERC20 TOKEN: This smart contract needs to keep track of token balances and spending allowances, 
+// as well as simple sending and receiving, and delegated sending and receiving, 
+// in order to allow it to be directly sendable between accounts, or tradable custodially on a decentralized exchange.
+// Notice that the name, symbol, and total supply of this ERC20 are programmed upon minting via the constructor.
 
 contract Token { // contains all smart contract code
     // name is invoked in hardhat using .name() function call after .getContractAt() function call
     // In ethereum, Token#name is a contract call
-    //string public name = "Gielda"; // state variable read from/stored directly on the blockchain
-    
-    //string public symbol = "GEDA";
 
+    // If i wanted to mint this token manually, I would declare the following without a constructor:
+    //string public name = "Gielda"; // state variable read from/stored directly on the blockchain
+    //string public symbol = "GEDA";
     //uint256 public totalSupply = 1000000 * (10**decimals); // Scientific Notation for WEI
 
-    uint256 public decimals = 18;
+    uint256 public decimals = 18; // smallest divisible value of the token
 
-    // give smart contract basic variable calls:
+    // Give the smart contract basic variable calls: (as opposed to function calls, these do not cost anything to view)
     string public name;
     string public symbol;
     uint256 public totalSupply;
     
-    // Track Balances (allow different addresses to have different balances of this token)
-    mapping(address => uint256) public balanceOf; //key-value pair 1-to-1 mapping
+    // Track balances (allow different addresses to have different balances of this token)
+    mapping(address => uint256) public balanceOf; // key-value pair 1-to-1 mapping
 
     // Track spending allowances
-    mapping(address => mapping(address => uint256)) public allowance; // returns all potential spenders and how much they are approved for
+    mapping(address => mapping(address => uint256)) public allowance; // returns all potential spenders for each address and how much they are approved for (1-to-pair mapping)
 
     event Transfer( 
         address indexed from, 
@@ -37,7 +40,7 @@ contract Token { // contains all smart contract code
         address indexed spender, 
         uint256 value); //events send notifications
 
-    // We use a constructor because the exchange needs to have the ability to mint token pairs
+    // We use a constructor because our dex exchange will need to have the ability to mint IOUs for token pairs
     constructor(
         string memory _name, 
         string memory _symbol,
@@ -49,12 +52,11 @@ contract Token { // contains all smart contract code
             balanceOf[msg.sender] = totalSupply; // Take all the tokens and assign them to deployment address
         }
 
-    // Internal transfer utility (private function)
+    // Internal transfer utility (private function) for use in our publically callable transfer functions
     function _transfer(
         address _from,
         address _to, 
-        uint256 _value
-    )
+        uint256 _value)
     internal {
         // Require that the receiver is not the 0 address
         require(_to != address(0)); // (prevents burning via the transfer function)
@@ -92,7 +94,7 @@ contract Token { // contains all smart contract code
         return true;
     }
 
-    // Allow delegated sending of tokens (after approval) by any token
+    // Allow delegated sending of tokens (after approval of the token holder) for any token holder
     function transferFrom(
         address _from,
         address _to, 
